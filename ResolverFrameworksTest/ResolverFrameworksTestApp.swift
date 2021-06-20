c8491ca285b9bf139792c4edcaa660e6f8057bd8
the.blaggy <the.blaggy@gmail.com>
2021-06-20 19:59:27 +0200

    Initial Commit

---
+++ ResolverFrameworksTest/ResolverFrameworksTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResolverFrameworksTestApp.swift
+//  ResolverFrameworksTest
+//
+//  Created by Lucas Tiedtke on 20.06.21.
+//
+
+import SwiftUI
+
+@main
+struct ResolverFrameworksTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
